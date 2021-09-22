ee90e3884583fac14f3a49e6d83430b4779be8b0
vappy2 <43212809+vappy2@users.noreply.github.com>
2021-09-22 15:54:18 +0200

    array roman + result
    On instancie le tableau des valeurs + une variable result pour y stocker le résultat

--- src/Exercises.php
+++ src/Exercises.php
@@ -41,6 +41,24 @@ class Exercises
 
     public static function romanToDecimal(string $romanNb) : int 
     {
+        $romanNumber = [
+            'M' => 1000,
+            'CM' => 900,
+            'D' => 500,
+            'CD' => 400,
+            'C' => 100,
+            'XC' => 90,
+            'X' => 50,
+            'XL' => 40,
+            'X' => 10,
+            'IX' => 9,
+            'V' => 5,
+            'IV' => 4,
+            'I' => 1
+        ];
+
+        $result = '';
+        
         if($romanNb == "I"){
             return 1;
         }
